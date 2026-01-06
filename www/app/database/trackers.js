799cd16f7e79ecbbe589ec4e76b34644676bcbf8
Michael Jedich <michael.jedich@pm.me>
2026-01-06 09:27:13 +0100

    Refactoring

--- www/app/database/trackers.js
+++ www/app/database/trackers.js
@@ -10,15 +10,15 @@ const STORAGE_KEY = 'trackers';
  * @param {Array<Tracker>} trackers
  * @returns {Promise<void>}
  */
-const serialize = async (trackers) => {
+async function serialize(trackers) {
     trackers = trackers.sort((a, b) => a.name.localeCompare(b.name));
     await storage.set(STORAGE_KEY, JSON.stringify(trackers));
-};
+}
 
 /**
  * @returns {Promise<Array<Tracker>>}
  */
-const deserialize = async () => {
+async function deserialize() {
     const entries = await storage.get(STORAGE_KEY);
     let trackers = (typeof entries === 'string') ? JSON.parse(entries) : [];
 
@@ -27,14 +27,14 @@ const deserialize = async () => {
     });
     result = await migrateTo110(result);
     return result;
-};
+}
 
 /**
  * @param {string} name
  * @param {string} name
  * @returns {Promise<Array<Tracker>>}
  */
-const add = async (name) => {
+async function add(name) {
     const trackers = await deserialize();
     if (trackers.find(entry => entry.name === name) != null) {
         return trackers;
@@ -42,29 +42,29 @@ const add = async (name) => {
     trackers.push(new Tracker(name));
     await serialize(trackers);
     return trackers;
-};
+}
 
 /**
  * @param {Tracker} tracker
  * @returns {Promise<Array<Tracker>>}
  */
-const update = async (tracker) => {
+async function update(tracker) {
     let trackers = await remove(tracker);
     trackers.push(tracker);
     await serialize(trackers);
     return trackers;
-};
+}
 
 /**
  * @param {Tracker} tracker
  * @returns {Promise<Array<Tracker>>}
  */
-const remove = async (tracker) => {
+async function remove(tracker) {
     let trackers = await deserialize();
     trackers = trackers.filter(entry => entry.created !== tracker.created);
     await serialize(trackers);
     return trackers;
-};
+}
 
 /**
  * Data migration: 1.0.3 -> 1.1.0
@@ -72,7 +72,7 @@ const remove = async (tracker) => {
  * @param {Array<Tracker>} trackers
  * @returns {Promise<Array<Tracker>>}
  */
-const migrateTo110 = async (trackers) => {
+async function migrateTo110(trackers) {
     for (let i in trackers) {
         const tracker = trackers[i];
         for (let date in tracker.dates) {
@@ -86,7 +86,7 @@ const migrateTo110 = async (trackers) => {
     }
     await serialize(trackers);
     return trackers;
-};
+}
 
 export const trackers = {
     list: deserialize,
